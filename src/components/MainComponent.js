6c6b8c0b349714e73c98a3b8e56f321c8d5c4e9c
Gene Harrel <generharrel@gmail.com>
2020-03-28 10:05:08 -0700

    React Router Parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -26,22 +26,21 @@ class Main extends Component {
 
     render() {
 
-        const HomePage = () => {
+        const CampsiteWithId = ({match}) => {
             return (
-                <Home
-                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
-                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
-                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                <CampsiteInfo 
+                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
                 />
             );
-        }
+        };   
 
         return (
             <div>
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
-                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Redirect to='/home' />
                     <Route exact path='/contactus' component={Contact} />
                 </Switch>
